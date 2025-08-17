70316f1932ee1e8c3405d14052fc2895e09c360a
Omm-codes <omsanjay975@gmail.com>
2025-08-17 23:56:34 +0530

    fix

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,21 +1,20 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
-  "stories": [
+  stories: [
     "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/preset-create-react-app",
     "@storybook/addon-docs",
-    "@storybook/addon-onboarding"
+    "@storybook/addon-onboarding",
   ],
-  "framework": {
-    "name": "@storybook/react-webpack5",
-    "options": {}
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
   },
-  "staticDirs": [
-    "..\\public"
-  ]
+  staticDirs: ["../public"], // ✅ fixed (use forward slashes)
 };
+
 export default config;
